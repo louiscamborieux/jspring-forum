05f59a0432264867e592514736d0ef63b05a94be
Louis Camborieux PC Portable <louis.camborieux@etu.iut-tlse3.fr>
2023-11-21 10:33:04 +0100

    Premier commit (hello world)

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/bonjour")
+	public String index() {
+		return "Bonjour tout le monde";
+	}
+
+}
